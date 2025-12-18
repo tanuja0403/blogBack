39a19ea4bb2dac7c6df8d7dd525bc94e6ba15aa6
Tanuja <tanuja048703@gmail.com>
2025-12-18 20:41:45 +0530

    Set up the basic workflow in it

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const commentRoutes = require("./routes/comment.routes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
