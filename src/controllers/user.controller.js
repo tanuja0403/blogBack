470f62a2c8c73ecf83ba667e7aa8cb29521eda75
Tanuja <tanuja048703@gmail.com>
2025-12-18 20:39:33 +0530

    Made controller for users

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,33 @@
+const prisma = require("../prismaClient");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password)
+    return res.status(400).json({ message: "All fields required" });
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  try {
+    await prisma.user.create({
+      data: { name, email, password: hashed },
+    });
+    res.json({ message: "User registered successfully" });
+  } catch {
+    res.status(400).json({ message: "Email already exists" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.status(401).json({ message: "Invalid credentials" });
+
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+  res.json({ token });
+};
